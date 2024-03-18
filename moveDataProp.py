1016c7642f8a69bbaa16f59c34ca9b6221f8c21e
mquantin <matthieu@tinta.fr>
2024-03-18 15:47:11 +0100

    less indent

--- moveDataProp.py
+++ moveDataProp.py
@@ -14,28 +14,28 @@ def moveDataProp(omeka, items, propFrom, propTo, delFrom = False):
     not_proc = []
     error = []
     for origItem in items['results']:
-        if propFrom in origItem:
-            print('processing item id n°',origItem['o:id'])
-            new_item = deepcopy(origItem)
-            #catches the content to be copied for each occurence in the propFrom of the item (maybe several values for the same prop)
-            for origPropValue in new_item[propFrom]:
-                origPropValueContent = None
-                if origPropValue['type'] == 'uri':
-                    origPropValueContent = origPropValue['@id']
-                    newPropvalue = {'value': origPropValueContent, 'type': 'uri', 'label': origPropValue['o:label']}
-                elif origPropValue['type'] == 'literal':
-                    origPropValueContent = origPropValue['@value']
-                    newPropvalue = {'value': origPropValueContent, 'type': 'literal'}
-                else:
-                    error.append(origItem['o:id']) 
-                    raise ValueError("property value is unclear, nor uri, nor litteral", origPropValue)
-                new_item = utils.add_to_prop(omeka, new_item, propTo_id, propTo, newPropvalue)
-            processed.append(origItem['o:id']) 
-            if delFrom:
-                del new_item[propFrom]
-                print('deleteing values of ', propFrom )
-            updated_item = omeka.update_resource(new_item, 'items')
-            assert origItem['o:id'] == updated_item['o:id']
-        else: 
+        if propFrom not in origItem:
             not_proc.append(origItem['o:id']) 
+            continue
+        print('processing item id n°',origItem['o:id'])
+        new_item = deepcopy(origItem)
+        #catches the content to be copied for each occurence in the propFrom of the item (maybe several values for the same prop)
+        for origPropValue in new_item[propFrom]:
+            origPropValueContent = None
+            if origPropValue['type'] == 'uri':
+                origPropValueContent = origPropValue['@id']
+                newPropvalue = {'value': origPropValueContent, 'type': 'uri', 'label': origPropValue['o:label']}
+            elif origPropValue['type'] == 'literal':
+                origPropValueContent = origPropValue['@value']
+                newPropvalue = {'value': origPropValueContent, 'type': 'literal'}
+            else:
+                error.append(origItem['o:id']) 
+                raise ValueError("property value is unclear, nor uri, nor litteral", origPropValue)
+            new_item = utils.add_to_prop(omeka, new_item, propTo_id, propTo, newPropvalue)
+        processed.append(origItem['o:id']) 
+        if delFrom:
+            del new_item[propFrom]
+            print('deleteing values of ', propFrom )
+        updated_item = omeka.update_resource(new_item, 'items')
+        assert origItem['o:id'] == updated_item['o:id']            
     return processed, not_proc, error
